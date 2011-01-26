f1e86a06df62cfbe3a15380a905dc9504995b853
Matt Revelle <mrevelle@gmail.com>
2011-01-25 19:58:45 -0800

    Formatting.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "General purpose functions for clj-sys projects"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [log4j/log4j "1.2.16"]]
+                 [log4j/log4j "1.2.16"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]])
