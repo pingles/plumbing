768d7322074319ccd78e5230df60645ad5b79d9e
Bradford <bradford@measuringmeasures.com>
2011-03-01 18:43:18 -0800

    merging in all serialization and messaging.

--- src/plumbing/serialize.clj
+++ src/plumbing/serialize.clj
@@ -2,9 +2,92 @@
   (:require [clj-json [core :as json]]
             [clojure.contrib.logging :as log])
   (:use	[clojure.contrib.def :only [defvar]]
+	[clojure.string :only [lower-case]]
 	[plumbing.serialize]
-	[plumbing.core :only [silent]])
-  (:import clojure.lang.RT))
+	[plumbing.core :only [silent]]
+	[clojure.contrib.server-socket :only [create-server
+                                              close-server]])
+  (:import clojure.lang.RT
+	   (java.net InetAddress Socket)
+	   (java.nio ByteBuffer)
+	   (java.util Arrays)
+	   (java.io InputStream OutputStream
+		    BufferedReader InputStreamReader
+		    PrintWriter)))
+
+(defn cr? [x]
+  (= 13 x))
+
+(defn nl? [x]
+  (= 10 x))
+
+(defn ubyte [x]
+  (if (> x 127)
+    (byte (- x 256))
+    (byte x)))
+
+(defn as-int [^Byte b]
+  (bit-and b 0xFF))
+
+(defn ^String read-ln [^InputStream is]
+  (let [bb (ByteBuffer/allocate 32)]
+    (loop [last-val 0]
+      (let [cur-val (as-int (.read is))]
+        (if (and (cr? last-val)
+                 (nl? cur-val))
+          (String. (Arrays/copyOf (.array bb)
+                                  (- (.position bb) 1)))
+          (do
+            (.put bb (ubyte cur-val))
+            (recur cur-val)))))))
+
+(defn arg-count? [^String x]
+  (.startsWith x "*"))
+
+(defn arg-len? [^String x]
+  (.startsWith x "$"))
+
+(defn read-arg-count [^InputStream is]
+  (let [l (read-ln is)]
+    (if (arg-count? l)
+      (Integer/parseInt (.substring l 1))
+      (throw (Exception. "Error parsing arg count.")))))
+
+(defn read-arg-len [^InputStream is]
+  (let [l (read-ln is)]
+    (if (arg-len? l)
+      (Integer/parseInt (.substring l 1))
+      (throw (Exception. "Error parsing arg length.")))))
+
+;; TODO: bubble up serialization
+(defn read-arg [^InputStream is]
+  (let [arg-len (read-arg-len is)
+        buf (byte-array arg-len)]
+    (.read is buf)
+    (.skip is 2)
+    (read-string (String. buf))))
+
+(defn read-msg [^InputStream is]
+  (let [arg-count (read-arg-count is)
+        args (repeatedly arg-count #(read-arg is))]
+    args))
+
+
+(defn write-arg-count [^OutputStream os c]
+  (.write os (.getBytes
+              (format "*%d\r\n" c))))
+
+;; TODO: bubble up serialization
+(defn write-arg [^OutputStream os ^String arg]
+  (let [arg-len (.length arg)]        
+    (.write os (.getBytes
+                (format "$%d\r\n%s\r\n"
+                        arg-len arg)))))
+
+(defn write-msg [^OutputStream os args]
+  (let [num-args (count args)]
+    (write-arg-count os num-args)
+    (doseq [arg args] (write-arg os (pr-str arg)))))
 
 (defprotocol Serializer
   (freeze [this obj] "freeze obj to bytes")
@@ -14,6 +97,42 @@
   Serializer
   (freeze [this obj] (.getBytes (prn-str obj)))
   (thaw [this bytes] (read-string (String. ^"[B" bytes))))
+
+(defn client-socket [^String host ^Integer port f]
+  (let [client (Socket. (InetAddress/getByName host) port)
+        os (.getOutputStream client)
+        ins (.getInputStream client)]
+    (f ins os)))
+
+(defn start [fun
+             & {:keys [port backlog bind-addr]
+                :or {port 4444
+                     backlog 50
+                     bind-addr (InetAddress/getByName "127.0.0.1")}}]
+  (let [server (create-server port fun backlog bind-addr)]
+    server))
+
+(defn req [cmd]
+  (fn [^InputStream ins
+       ^OutputStream os]
+    (write-msg os cmd)
+    (-> (read-msg ins)
+        first)))
+
+(defn handler [f]
+  "Map of buckets."
+  (fn [^InputStream is ^OutputStream os]
+    (let [i (read-msg is)]
+      (write-msg os (f i))
+      (.flush os))))
+
+(defn server [f ^InputStream is ^OutputStream os]
+  (let [reader (BufferedReader. (InputStreamReader. is))
+	writer (PrintWriter. os)]
+    (let [input (read-string (.readLine reader))
+	     resp (f input)]
+      (.println os (pr-str resp))
+      (.flush os))))
 
 (def default-serializer   (DefaultStringSerializer.))
 
--- test/plumbing/serialize_test.clj
+++ test/plumbing/serialize_test.clj
@@ -1,5 +1,52 @@
 (ns plumbing.serialize-test
-  (:use plumbing.serialize clojure.test))
+  (:use plumbing.serialize
+	clojure.test)
+  (:import (org.apache.commons.io IOUtils)
+           (java.io ByteArrayOutputStream)))
+
+(deftest read-ln-test
+  (is (= "GET"
+         (read-ln (IOUtils/toInputStream "GET\r\n")))))
+
+(deftest read-arg-count-test
+  (is (= 2
+         (read-arg-count (IOUtils/toInputStream
+                          "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n")))))
+
+(deftest read-arg-len-test
+  (is (= 3
+         (read-arg-len (IOUtils/toInputStream
+                        "$3\r\nGET\r\n$3\r\nkey\r\n")))))
+
+(deftest read-arg-test
+  (is (= (seq "GET")
+         (seq (read-arg (IOUtils/toInputStream
+                         "$5\r\n\"GET\"\r\n$3\r\nkey\r\n"))))))
+
+(deftest read-msg-test
+  (let [[cmd & args] (read-msg (IOUtils/toInputStream
+                                "*2\r\n$5\r\n\"GET\"\r\n$5\r\n\"key\"\r\n"))]
+    (is (= "GET" cmd))
+    (is (= (list "key")
+           args))))
+
+(deftest write-arg-count-test
+  (let [baos (doto (ByteArrayOutputStream.)
+               (write-arg-count 20))]
+    (is (= "*20\r\n"
+           (String. (.toByteArray baos))))))
+
+(deftest write-arg-test
+  (let [baos (doto (ByteArrayOutputStream.)
+               (write-arg (pr-str 1024)))]
+    (is (= "$4\r\n1024\r\n"
+           (String. (.toByteArray baos))))))
+
+(deftest write-msg-test
+  (let [baos (doto (ByteArrayOutputStream.)
+               (write-msg [555 "rawr"]))]
+    (is (= "*2\r\n$3\r\n555\r\n$6\r\n\"rawr\"\r\n"
+           (String. (.toByteArray baos))))))
 
 (defn- round-trip-test [serialize-impl s]
   (is (= s (thaw serialize-impl (freeze serialize-impl s)))))
