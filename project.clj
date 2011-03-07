4b74b1777a66cf019c15cbc913d64b75becf4fad
Matt Revelle <mrevelle@gmail.com>
2011-03-06 19:57:41 -0800

    Updated clj-json deps.

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "General purpose functions for clj-sys projects"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [woven/clj-json "0.3.1"]
+                 [woven/clj-json "0.3.2"]
                  [log4j/log4j "1.2.16"]
-		 [commons-io "2.0.1"]]
+                 [commons-io "2.0.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"]]
   :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"
